ac2d2ffa558bf40df3b720569bfb8b7333db38a7
Muhammad Iqbal <webcracking@gmail.com>
2022-10-26 12:26:17 +0800

    tambah bbrp fungsi

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,35 +26,35 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-		$this->data['web_title'] = Setting::firstOrCreate(['nama_setting' => 'web_title'], ['isi_setting' => 'Kalla Institute'])->isi_setting;
-		$this->data['web_logo'] = Setting::firstOrCreate(['nama_setting' => 'web_logo'], ['isi_setting' => 'https://i.postimg.cc/ZK5qzGVV/kalla-group.png'])->isi_setting;
-		$this->data['web_icon'] = Setting::firstOrCreate(['nama_setting' => 'web_icon'], ['isi_setting' => null])->isi_setting;
-		$this->data['web_description'] = Setting::firstOrCreate(['nama_setting' => 'web_description'], ['isi_setting' => null])->isi_setting;
-		$this->data['web_keywords'] = Setting::firstOrCreate(['nama_setting' => 'web_keywords'], ['isi_setting' => null])->isi_setting;
-		$this->data['theme_color'] = Setting::firstOrCreate(['nama_setting' => 'theme_color'], ['isi_setting' => null])->isi_setting;
-        view()->composer('components.admin-layout', function($view)
-		{
-			$view->with(['web_title' => $this->data['web_title'], 'web_icon' => $this->data['web_icon'], 'web_description' => $this->data['web_description'], 'web_keywords' => $this->data['web_keywords']]);
-		});
+		// $this->data['web_title'] = Setting::firstOrCreate(['nama_setting' => 'web_title'], ['isi_setting' => 'Kalla Institute'])->isi_setting;
+		// $this->data['web_logo'] = Setting::firstOrCreate(['nama_setting' => 'web_logo'], ['isi_setting' => 'https://i.postimg.cc/ZK5qzGVV/kalla-group.png'])->isi_setting;
+		// $this->data['web_icon'] = Setting::firstOrCreate(['nama_setting' => 'web_icon'], ['isi_setting' => null])->isi_setting;
+		// $this->data['web_description'] = Setting::firstOrCreate(['nama_setting' => 'web_description'], ['isi_setting' => null])->isi_setting;
+		// $this->data['web_keywords'] = Setting::firstOrCreate(['nama_setting' => 'web_keywords'], ['isi_setting' => null])->isi_setting;
+		// $this->data['theme_color'] = Setting::firstOrCreate(['nama_setting' => 'theme_color'], ['isi_setting' => null])->isi_setting;
+        // view()->composer('components.admin-layout', function($view)
+		// {
+			// $view->with(['web_title' => $this->data['web_title'], 'web_icon' => $this->data['web_icon'], 'web_description' => $this->data['web_description'], 'web_keywords' => $this->data['web_keywords']]);
+		// });
 		
-		view()->composer('components.sidebar', function($view)
-		{
-			$view->with(['web_logo' => $this->data['web_logo'], 'theme_color' => $this->data['theme_color']]);
-		});
+		// view()->composer('components.sidebar', function($view)
+		// {
+			// $view->with(['web_logo' => $this->data['web_logo'], 'theme_color' => $this->data['theme_color']]);
+		// });
 		
-		view()->composer('components.header', function($view)
-		{
-			$view->with('web_logo', $this->data['web_logo']);
-		});
+		// view()->composer('components.header', function($view)
+		// {
+			// $view->with('web_logo', $this->data['web_logo']);
+		// });
 		
-		view()->composer('layouts.app', function($view)
-		{
-			$view->with(['web_title' => $this->data['web_title'], 'web_icon' => $this->data['web_icon'], 'web_description' => $this->data['web_description'], 'web_keywords' => $this->data['web_keywords']]);
-		});
+		// view()->composer('layouts.app', function($view)
+		// {
+			// $view->with(['web_title' => $this->data['web_title'], 'web_icon' => $this->data['web_icon'], 'web_description' => $this->data['web_description'], 'web_keywords' => $this->data['web_keywords']]);
+		// });
 		
-		view()->composer('layouts.nav', function($view)
-		{
-			$view->with('web_logo', $this->data['web_logo']);
-		});
+		// view()->composer('layouts.nav', function($view)
+		// {
+			// $view->with('web_logo', $this->data['web_logo']);
+		// });
     }
 }
