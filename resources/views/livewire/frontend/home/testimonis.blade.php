d203062ca8c4edb32b372a80c453e353f26fbcaf
Muhammad Iqbal <webcracking@gmail.com>
2022-11-13 08:45:52 +0800

    change testimoni view

--- resources/views/livewire/frontend/home/testimonis.blade.php
+++ resources/views/livewire/frontend/home/testimonis.blade.php
@@ -1,32 +1,318 @@
 <div>
-	@if(count($data['testimonis']) >= 2)
-    <section class="probootstrap-section probootstrap-bg probootstrap-section-colored probootstrap-testimonial" style="background-image: url({{asset('frontend/assets/images/slider_2.jpg')}});background-size: cover;padding:3em 0;">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-6 col-md-offset-3 text-center section-heading probootstrap-animate">
-              <h2>TESTIMONI</h2>
-              <p class="lead">Apa kata mereka?</p>
-            </div>
-          </div>
-          <!-- END row -->
-          <div class="row">
-            <div class="col-md-12 probootstrap-animate">
-              <div class="owl-carousel owl-carousel-testimony owl-carousel-fullwidth">
-				@foreach($data['testimonis'] as $testimoni)
-                <div class="item">
-                  <div class="probootstrap-testimony-wrap text-center">
-                    <figure>
-                      <img src="{{asset('storage/images/'.$testimoni->gambar)}}" alt="$testimoni->nama">
-                    </figure>
-                    <blockquote class="quote">&ldquo;{{$testimoni->deskripsi}}&rdquo; <cite class="author"> &mdash; <span>{{$testimoni->nama}}</span></cite></blockquote>
-                  </div>
+	<section class="testimonial-section">
+        <!-- Swiper -->
+        <div class="swiper-container testimonial-slider swiper-container-fade swiper-container-horizontal swiper-container-wp8-horizontal">
+            <div class="swiper-wrapper" style="transition-duration: 0ms;"><div class="swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="2" style="width: 1349px; opacity: 1; transform: translate3d(0px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 flex order-2 order-lg-1 align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="https://technext.github.io/ezuca/images/user-3.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100 isi_testimoni">
+                                <div class="entry-content">
+                                    <p>Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">James Stephens - <span>University in Cambridge</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
                 </div>
-                @endforeach
-              </div>
-            </div>
-          </div>
-          <!-- END row -->
-        </div>
+                <div class="swiper-slide swiper-slide-duplicate-next" data-swiper-slide-index="0" style="width: 1349px; opacity: 1; transform: translate3d(-1349px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 order-2 order-lg-1 flex align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="https://technext.github.io/ezuca/images/user-1.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100 isi_testimoni">
+                                <div class="entry-content">
+                                    <p>Together as teachers, students and universities we can help make this education available for everyone.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">Russell Stephens - <span>University in UK</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div><!-- .swiper-slide -->
+
+                <div class="swiper-slide swiper-slide-prev" data-swiper-slide-index="1" style="width: 1349px; opacity: 1; transform: translate3d(-2698px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 order-2 order-lg-1 flex align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="https://technext.github.io/ezuca/images/user-2.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100 isi_testimoni">
+                                <div class="entry-content">
+                                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">Robert Stephens - <span>University in Oxford</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div><!-- .swiper-slide -->
+
+                <div class="swiper-slide swiper-slide-active" data-swiper-slide-index="2" style="width: 1349px; opacity: 1; transform: translate3d(-4047px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 flex order-2 order-lg-1 align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="https://technext.github.io/ezuca/images/user-3.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100">
+                                <div class="entry-content">
+                                    <p>Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">James Stephens - <span>University in Cambridge</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div><!-- .swiper-slide -->
+            <div class="swiper-slide swiper-slide-duplicate swiper-slide-next" data-swiper-slide-index="0" style="width: 1349px; opacity: 0; transform: translate3d(-5396px, 0px, 0px); transition-duration: 0ms;">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12 col-lg-6 order-2 order-lg-1 flex align-items-center mt-5 mt-lg-0">
+                                <figure class="user-avatar">
+                                    <img src="images/user-1.jpg" alt="">
+                                </figure><!-- .user-avatar -->
+                            </div><!-- .col -->
+
+                            <div class="col-12 col-lg-6 order-1 order-lg-2 content-wrap h-100">
+                                <div class="entry-content">
+                                    <p>Together as teachers, students and universities we can help make this education available for everyone.</p>
+                                </div><!-- .entry-content -->
+
+                                <div class="entry-footer">
+                                    <h3 class="testimonial-user">Russell Stephens - <span>University in UK</span></h3>
+                                </div><!-- .entry-footer -->
+                            </div><!-- .col -->
+                        </div><!-- .row -->
+                    </div><!-- .container -->
+                </div></div><!-- .swiper-wrapper -->
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-lg-6 mt-5 mt-lg-0">
+                        <div class="swiper-pagination position-relative flex justify-content-center align-items-center swiper-pagination-clickable swiper-pagination-bullets"><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet"></span><span class="swiper-pagination-bullet swiper-pagination-bullet-active"></span></div>
+                    </div><!-- .col -->
+                </div><!-- .row -->
+            </div><!-- .container -->
+        </div><!-- .testimonial-slider -->
     </section>
-	@endif
+	<!-- Swiper CSS -->
+    <link rel="stylesheet" href="{{asset('frontend/assets/css/swiper.css')}}">
+    <!--link rel="stylesheet" href="{{asset('frontend/assets/css/bootstrap.css')}}"-->
+	<style>
+	
+	@media (min-width: 1200px)
+	.container {
+		max-width: 1140px;
+	}
+	@media (min-width: 992px)
+	.container {
+		max-width: 960px;
+	}
+
+	@media (min-width: 768px)
+	.container {
+		max-width: 720px;
+	}
+	@media (min-width: 576px)
+	.container {
+		max-width: 540px;
+	}
+	.container {
+		width: 100%;
+		padding-right: 15px;
+		padding-left: 15px;
+		margin-right: auto;
+		margin-left: auto;
+	}
+	*, ::after, ::before {
+		box-sizing: border-box;
+	}
+	.row {
+		display: -ms-flexbox;
+		display: flex;
+		-ms-flex-wrap: wrap;
+		flex-wrap: wrap;
+		margin-right: -15px;
+		margin-left: -15px;
+	}
+	
+	.position-relative {
+		position: relative!important;
+	}
+	.align-items-center {
+		-ms-flex-align: center!important;
+		align-items: center!important;
+	}
+	.justify-content-center {
+		-ms-flex-pack: center!important;
+		justify-content: center!important;
+	}
+
+	/*
+	# About Section: Video
+	--------------------------------*/
+	.ezuca-video {
+		width: 100%;
+	}
+
+	.ezuca-video .video-play-btn {
+		top: 50%;
+		left: -40px;
+		z-index: 99;
+		width: 80px;
+		height: 80px;
+		margin-top: -40px;
+		cursor: pointer;
+	}
+
+	.ezuca-video img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	@media screen and (max-width: 992px) {
+		.about-section .heading {
+			padding-right: 0;
+		}
+
+		.ezuca-stats {
+			padding-right: 0;
+		}
+
+		.ezuca-video .video-play-btn {
+			left: 50%;
+			margin-left: -40px;
+		}
+	}
+
+	/*
+	# About Section: Testimonial
+	--------------------------------*/
+	.testimonial-section {
+		padding: 100px 0;
+		background: #F7F7F7;
+	}
+
+	.testimonial-slider .swiper-slide .user-avatar {
+		opacity: .3;
+		margin-bottom: 0;
+	}
+
+	.testimonial-slider .swiper-slide.swiper-slide-active .user-avatar {
+		opacity: 1;
+	}
+
+	.testimonial-slider .swiper-slide:nth-of-type(2) .user-avatar,
+	.testimonial-slider .swiper-slide:nth-of-type(5) .user-avatar {
+		margin-left: calc(50% - 150px);
+	}
+
+	.testimonial-slider .swiper-slide:nth-of-type(3) .user-avatar {
+		margin-left: calc(50% - 40px);
+	}
+
+	.testimonial-slider .swiper-slide:nth-of-type(1) .user-avatar,
+	.testimonial-slider .swiper-slide:nth-of-type(4) .user-avatar {
+		margin-left: calc(50% + 70px);
+	}
+
+
+	.testimonial-slider .swiper-slide-next {
+		opacity: 1 !important;
+	}
+
+	.testimonial-slider .user-avatar {
+		width: 80px;
+		height: 80px;
+		border-radius: 50%;
+		overflow: hidden;
+	}
+
+	.testimonial-slider .user-avatar img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	.testimonial-slider .content-wrap {
+		background: #fff;
+		padding: 20px;
+		border-radius: 30px;
+	}
+
+	.testimonial-slider .entry-content p {
+		font-size: 26px;
+		color: #383749;
+	}
+
+	.testimonial-slider .testimonial-user {
+		display: inline-block;
+		position: relative;
+		font-size: 16px;
+		font-weight: 300;
+		text-transform: uppercase;
+		color: #19c880;
+	}
+
+	.testimonial-slider .testimonial-user::after {
+		content: '';
+		position: absolute;
+		top: -12px;
+		right: -100px;
+		width: 38px;
+		height: 32px;
+		background: url("https://technext.github.io/ezuca/images/quote-icon.png") no-repeat center;
+	}
+
+	.testimonial-slider .testimonial-user span {
+		font-size: 14px;
+		color: #c0c1cd;
+		text-transform: initial;
+	}
+
+	.testimonial-slider .swiper-pagination-bullet {
+		width: 12px;
+		height: 12px;
+		margin: 0 6px;
+		border: 2px solid #c6adb8;
+		background: transparent;
+		opacity: 1;
+	}
+
+	.testimonial-slider .swiper-pagination-bullet.swiper-pagination-bullet-active {
+		background: #19c880;
+		border: transparent;
+	}
+	
+	</style>
+	<script type="text/javascript" src="{{asset('frontend/assets/js/jquery.js')}}"></script>
+	<script type="text/javascript" src="{{asset('frontend/assets/js/swiper.min.js')}}"></script>
+	<!--script type="text/javascript" src="{{asset('frontend/assets/js/masonry.pkgd.min.js')}}"></script>
+	<script type="text/javascript" src="{{asset('frontend/assets/js/jquery.collapsible.min.js')}}"></script-->
+	<script type="text/javascript" src="{{asset('frontend/assets/js/custom-ezuca.js')}}"></script>
 </div>
