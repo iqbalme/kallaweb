6a8b9079fae318899a017b44703b84eed8cf0a40
Muhammad Iqbal <webcracking@gmail.com>
2022-11-15 07:08:47 +0800

    fix daftar event

--- resources/views/livewire/frontend/show-event-single.blade.php
+++ resources/views/livewire/frontend/show-event-single.blade.php
@@ -150,7 +150,7 @@
 		</div>
 	  </div>
 	</div>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"> -->
 	<script>
 		function showEventRegistration(){
 			jQuery("#daftarEvent").modal('show');
