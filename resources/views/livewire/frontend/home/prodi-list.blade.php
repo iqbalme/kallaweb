f585161a59e72862a8667567a7604d2f26caa3e0
Muhammad Iqbal <webcracking@gmail.com>
2022-11-19 15:04:50 +0800

    fix errors

--- resources/views/livewire/frontend/home/prodi-list.blade.php
+++ resources/views/livewire/frontend/home/prodi-list.blade.php
@@ -1,32 +1,36 @@
 <div>
 	@if(isset($data['prodis']))
     <div class="prodi">
-		<div class="container">
-			<div class="row">
-				<div class="col">
-					<div class="blog_prodi_container" style="position:relative;height:auto;">
+		<div class="container overflow-hidden">
+			<div class="row justify-content-between">
+				
+					<!--div class="blog_prodi_container" style="position:relative;height:auto;"-->
+					<!--div class="col-lg-6"-->
 							@foreach($data['prodis'] as $prodi)
-								<div class="blog_prodi trans_200" style="position:relative;" id="post-{{$loop->iteration}}" data-index="{{$loop->iteration}}">
-									@isset($prodi->thumbnail)
-										<a href="{{route('post.single', ['post_val' => $prodi->slug])}}"><div class="blog_prodi_image" style="background-image:url('{{asset('storage/images/'.$prodi->thumbnail)}}');background-size:cover;"></div></a>
-									@endisset
-									<div class="blog_post_body">
-										<div class="blog_post_title text-center"><a href="#">{{$prodi->nama_prodi}}</a></div>
-										<div class="blog_post_text">
-										{{substr($prodi->deskripsi_prodi,0,77).'...'}}
+								<!--div class="blog_prodi trans_200" style="position:relative;" id="post-{{$loop->iteration}}" data-index="{{$loop->iteration}}"-->
+								<div class="col-lg-3 col-md-6 col-sm-12 col-12 trans_200 p-2">
+									<div class="blog_prodi_list">
+										@isset($prodi->thumbnail)
+											<a href="{{route('post.single', ['post_val' => $prodi->slug])}}"><div class="blog_prodi_image" style="background-image:url('{{asset('storage/images/'.$prodi->thumbnail)}}');background-size:cover;"></div></a>
+										@endisset
+										<div class="blog_post_body">
+											<div class="blog_post_title text-center"><a href="#">{{$prodi->nama_prodi}}</a></div>
+											<div class="blog_post_text">
+											{{substr($prodi->deskripsi_prodi,0,77).'...'}}
+											</div>
 										</div>
 									</div>
 								</div>
 							@endforeach
-					</div>
-				</div>
+					<!--/div-->
+				
 			</div>
 		</div>
 	</div>
 	<script>
 		jQuery( document ).ready(function() {
 			// jQuery(".blog_prodi_image").height(jQuery(".blog_prodi").width());
-			jQuery(".blog_prodi_image").height(jQuery(".blog_prodi").width()*0.53);
+			jQuery(".blog_prodi_image").height(jQuery(".blog_prodi_list").width()*0.53);
 		});
 	</script>
 	<link href="{{asset('frontend/assets/css/kalla-style.css')}}" rel="stylesheet" type="text/css">	
