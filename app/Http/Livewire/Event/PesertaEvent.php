3589bc3b8a0c288fe72b7ed184917b464d1fb8f4
Muhammad Iqbal <webcracking@gmail.com>
2022-11-29 13:13:12 +0800

    fix error

--- app/Http/Livewire/Event/PesertaEvent.php
+++ app/Http/Livewire/Event/PesertaEvent.php
@@ -3,7 +3,7 @@
 namespace App\Http\Livewire\Event;
 
 use Livewire\Component;
-use App\Models\PesertaEvent;
+use App\Models\PesertaEvent as peserta_event;
 
 class PesertaEvent extends Component
 {
@@ -12,7 +12,7 @@ class PesertaEvent extends Component
 	protected $listeners = ['get_peserta_event'];
 	
 	public function mount($event_id){
-		$this->data = PesertaEvent::where('event_id', $event_id)->orderBy('nama', 'asc')->paginate(10);
+		$this->data = peserta_event::where('event_id', $event_id)->orderBy('nama', 'asc')->paginate(10);
 	}
 	
     public function render()
