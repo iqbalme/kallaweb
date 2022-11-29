2623c3194160457ced2f269e11cd8c684de45540
Muhammad Iqbal <webcracking@gmail.com>
2022-11-29 13:15:40 +0800

    fix error

--- app/Http/Livewire/Event/PesertaEvent.php
+++ app/Http/Livewire/Event/PesertaEvent.php
@@ -8,8 +8,7 @@ use App\Models\PesertaEvent as peserta_event;
 class PesertaEvent extends Component
 {
 	public $data;
-	
-	protected $listeners = ['get_peserta_event'];
+	public $numberOfPaginatorsRendered;
 	
 	public function mount($event_id){
 		$this->data = peserta_event::where('event_id', $event_id)->orderBy('nama', 'asc')->paginate(10);
@@ -19,13 +18,5 @@ class PesertaEvent extends Component
     {
         return view('livewire.event.peserta-event');
     }
-	
-	public function get_peserta_event($event_id){
-		// $event = Event::find($event_id);
-		// dd($event->peserta_event);
-		// foreach($event->peserta_event->get() as $peserta){
-			// $data[] = $peserta;
-		// }
-		// $this->data = $data;
-	}
+
 }
