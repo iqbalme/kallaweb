6d96193b8dda4c8594a7fbd2c09eb98b5dadba9d
Muhammad Iqbal <webcracking@gmail.com>
2022-12-06 23:57:25 +0800

    add try catch on export module

--- app/Http/Livewire/Admin/PendaftarCtrl.php
+++ app/Http/Livewire/Admin/PendaftarCtrl.php
@@ -23,17 +23,20 @@ class PendaftarCtrl extends Component
 
     public function exportPendaftar(){
         $pendaftar = Pendaftar::where('aktif', 1)->orderBy('created_at', 'asc')->get();
-        dd($pendaftar);
-        (new FastExcel($pendaftar))->export('calon-mahasiswa-kalla-institute.xlsx', function ($camaba) {
-            return [
-                'Nama Lengkap' => strtoupper($camaba->nama),
-                'Email' => $camaba->email,
-                'No. KTP' => $camaba->no_ktp,
-                'No. HP' => $camaba->hp,
-                'Asal Sekolah' => $camaba->asal_sekolah,
-                'Program Studi' => strtoupper($camaba->nama_prodi),
-                'Waktu Pendaftaran' => $camaba->created_at->format('d-m-Y H:i')
-            ];
-        });
+        try {
+            (new FastExcel($pendaftar))->export('calon-mahasiswa-kalla-institute.xlsx', function ($camaba) {
+                return [
+                    'Nama Lengkap' => strtoupper($camaba->nama),
+                    'Email' => $camaba->email,
+                    'No. KTP' => $camaba->no_ktp,
+                    'No. HP' => $camaba->hp,
+                    'Asal Sekolah' => $camaba->asal_sekolah,
+                    'Program Studi' => strtoupper($camaba->nama_prodi),
+                    'Waktu Pendaftaran' => $camaba->created_at->format('d-m-Y H:i')
+                ];
+            });
+        } catch(\Exception $e){
+            dd($e->getMessage());
+        }
     }
 }
