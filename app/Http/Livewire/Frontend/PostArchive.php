a0b28ecd97c921a004c10761809fd2833106c00c
Muhammad Iqbal <webcracking@gmail.com>
2022-12-17 17:35:58 +0800

    fix post archive

--- app/Http/Livewire/Frontend/PostArchive.php
+++ app/Http/Livewire/Frontend/PostArchive.php
@@ -5,16 +5,30 @@ namespace App\Http\Livewire\Frontend;
 use Livewire\Component;
 use App\Models\Post;
 use Livewire\WithPagination;
+use Illuminate\Http\Request;
 
 class PostArchive extends Component
 {
 	use WithPagination;
 	public $data;
-	
+    public $initial_data_req = null;
+
+    public function mount(Request $request){
+        $this->initial_data_req = $request->request->all();
+    }
+
     public function render()
     {
-		$this->data['posts'] = Post::orderByDesc('created_at')->where('status_post', 'published')->paginate(9);
-		//dd($this->data['posts']);
+        if($this->initial_data_req['is_main_domain']){
+		    $this->data['posts'] = Post::orderByDesc('created_at')->where('status_post', 'published')->paginate(9);
+        } else {
+            $ids_post = [];
+            $post_ids = PostProdis::where('prodi_id', $this->initial_data_req['subdomain']['id'])->get();
+            foreach($post_ids as $ids){
+                $ids_post[] = $ids->post_id;
+            }
+            $this->data['posts'] = Post::orderByDesc('created_at')->where('status_post', 'published')->whereIn('id', $ids_post)->paginate(9);
+        }
         return view('livewire.frontend.post-archive')
 			->extends('layouts.app', ['title' => 'List Berita'])
 			->section('content');
